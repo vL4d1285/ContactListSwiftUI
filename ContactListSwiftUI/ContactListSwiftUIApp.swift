e01ef9e84e659fb975e766a5fc1b0ab272fd6052
Vladislav Ryabtsev <vryabtsev@me.com>
2022-02-04 13:24:15 +0300

    Initial Commit

---
+++ ContactListSwiftUI/ContactListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListSwiftUIApp.swift
+//  ContactListSwiftUI
+//
+//  Created by Vladislav Ryabtsev on 04.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct ContactListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
